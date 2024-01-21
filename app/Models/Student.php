5057df8b4d3afc51c7ab969944ce1bb0aab7c20c
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2024-01-21 10:51:17 +0530

    Revert "Separated student and counselor dashboards"
    This reverts commit e8d2b954428fbea87cd7efec132b69b98f4cd70c.

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,51 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class Student extends Model implements Authenticatable
+class Student extends Model
 {
-    use Notifiable;
-
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    // Authenticatable interface methods
-    public function getAuthIdentifierName()
-    {
-        return 'id';
-    }
-
-    public function getAuthIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
-
-    public function getRememberToken()
-    {
-        return $this->remember_token;
-    }
-
-    public function setRememberToken($value)
-    {
-        $this->remember_token = $value;
-    }
-
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
 }
-
 
